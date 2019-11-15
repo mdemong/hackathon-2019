02ef1f975fab3a83b6d131afef50c2a341f3131b
Micah Demong <mdemong1@gmail.com>
2019-11-15 15:50:04 -0800

    computer vision works!

--- vision.js
+++ vision.js
@@ -1,18 +1,18 @@
-quickstart();
+getCards('./biology6.jpg');
 
-async function quickstart() {
+async function label() {
     // Imports the Google Cloud client library
     const vision = require('@google-cloud/vision');
-  
+
     // Creates a client
     const client = new vision.ImageAnnotatorClient();
-  
+
     // Performs label detection on the image file
-    const [result] = await client.labelDetection('./hackathon2.jpg');
+    const [result] = await client.textDetection('./hackathon2.jpg');
     const labels = result.labelAnnotations;
     console.log('Labels:');
     labels.forEach(label => console.log(label.description));
-  }
+}
 
 /**
   * From https://stackoverflow.com/a/14731922
@@ -26,9 +26,58 @@ async function quickstart() {
   * @param {Number} maxHeight maximum available height
   * @return {Object} { width, height }
   */
- function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
+function calculateAspectRatioFit(srcWidth, srcHeight, maxWidth, maxHeight) {
 
     var ratio = Math.min(maxWidth / srcWidth, maxHeight / srcHeight);
 
-    return { width: srcWidth*ratio, height: srcHeight*ratio };
- }
+    return { width: srcWidth * ratio, height: srcHeight * ratio };
+}
+
+async function text() {
+    const vision = require('@google-cloud/vision');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+    const fileName = './hackathon2.jpg';
+
+    // Performs text detection on the local file
+    const [result] = await client.textDetection(fileName);
+    const detections = result.textAnnotations;
+    console.log('Text:');
+    // detections.forEach(text => console.log(text));
+    console.log(detections[0]['description']);
+}
+
+async function getCards(fileName) {
+
+    // Imports the Google Cloud client library
+    const vision = require('@google-cloud/vision');
+
+    // Creates a client
+    const client = new vision.ImageAnnotatorClient();
+
+
+    // Read a local image as a text document
+    const [result] = await client.documentTextDetection(fileName);
+    const fullTextAnnotation = result.fullTextAnnotation;
+    console.log(`Full text:\n\n ${fullTextAnnotation.text}`);
+    fullTextAnnotation.pages.forEach(page => {
+        page.blocks.forEach(block => {
+            console.log(`Block confidence: ${block.confidence}`);
+            block.paragraphs.forEach(paragraph => {
+                console.log(`Paragraph confidence: ${paragraph.confidence}`);
+                paragraph.words.forEach(word => {
+                    const wordText = word.symbols.map(s => s.text).join('');
+                    console.log(`Word text: ${wordText}`);
+                    console.log(`Word confidence: ${word.confidence}`);
+                    // word.symbols.forEach(symbol => {
+                    //     console.log(`Symbol text: ${symbol.text}`);
+                    //     console.log(`Symbol confidence: ${symbol.confidence}`);
+                    // });
+                });
+            });
+        });
+    });
+
+
+}
